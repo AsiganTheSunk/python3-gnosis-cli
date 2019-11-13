5755ed8997e3785938678c432aef266cf96f7a2b
PedroAriasRuiz <asiganthesunk@gmail.com>
2019-11-13 11:44:39 +0100

    Rename default_messages to default values

--- core/gnosis_console_input.py
+++ core/gnosis_console_input.py
@@ -46,47 +46,39 @@ class GnosisConsoleInput:
         self.logger.addHandler(console_handler)
         pass
 
-# gnosis_safe_cli_completer = WordCompleter([
-#     'safe_addr', 'add', 'after', 'all', 'before', 'check', 'current_date',
-#     'current_time', 'current_timestamp', 'default',
-#     'delete', 'without'], ignore_case=True)
-#
-# style = Style.from_dict({
-#     'completion-menu.completion': 'bg:#008888 #ffffff',
-#     'completion-menu.completion.current': 'bg:#00aaaa #000000',
-#     'scrollbar.background': 'bg:#88aaaa',
-#     'scrollbar.button': 'bg:#222222',
-# })
-#
-# def main():
-#     # connection = sqlite3.connect(database)
-#     session = PromptSession(
-#         lexer=PygmentsLexer(SqlLexer), completer=gnosis_safe_cli_completer, style=style)
-#
-#     while True:
-#         try:
-#             text = session.prompt(' (gnosis-safe-manager-cli)> ')
-#         except KeyboardInterrupt:
-#             continue  # Control-C pressed. Try again.
-#         except EOFError:
-#             break  # Control-D pressed.
-#
-#         # with connection:
-#         #     try:
-#         #         messages = connection.execute(text)
-#         #     except Exception as e:
-#         #         print(repr(e))
-#         #     else:
-#         #         for message in messages:
-#         #             print(message)
-#
-#     print('GoodBye!')
-#
-#
-# if __name__ == '__main__':
-#     if len(sys.argv) < 2:
-#         db = ':memory:'
-#     else:
-#         db = sys.argv[1]
-#
-#     main(db)
+gnosis_safe_cli_completer = WordCompleter([
+    'safe_addr', 'add', 'after', 'all', 'before', 'check', 'current_date',
+    'current_time', 'current_timestamp', 'default',
+    'delete', 'without'], ignore_case=True)
+
+style = Style.from_dict({
+    'completion-menu.completion': 'bg:#008888 #ffffff',
+    'completion-menu.completion.current': 'bg:#00aaaa #000000',
+    'scrollbar.background': 'bg:#88aaaa',
+    'scrollbar.button': 'bg:#222222',
+})
+
+def main():
+    # connection = sqlite3.connect(database)
+    session = PromptSession(lexer=PygmentsLexer(SqlLexer), completer=gnosis_safe_cli_completer, style=style)
+    while True:
+        try:
+            text = session.prompt(' (gnosis-safe-manager-cli)> ')
+        except KeyboardInterrupt:
+            continue  # Control-C pressed. Try again.
+        except EOFError:
+            break  # Control-D pressed.
+
+        # with connection:
+        #     try:
+        #         messages = connection.execute(text)
+        #     except Exception as e:
+        #         print(repr(e))
+        #     else:
+        #         for message in messages:
+        #             print(message)
+
+    print('GoodBye!')
+
+if __name__ == '__main__':
+    main()
