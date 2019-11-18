b94875d3808d3cf3b02c9c94c470e2f1308a326d
PedroAriasRuiz <asiganthesunk@gmail.com>
2019-11-19 00:29:36 +0100

    Add deterministic mnemonic to ganache_constants

--- core/providers/constants/ganache_constants.py
+++ core/providers/constants/ganache_constants.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+DETERMINISTIC_MNEMONIC = 'myth like bonus scare over problem client lizard pioneer submit female collect'
+
 DETERMINISTIC_ACCOUNT_INFORMATION = {
             'account_0': {
                 'address': '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1',
